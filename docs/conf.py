fc5d6074088d2eceaac64de3a5c1d8f78712388a
Daniel <cdanielmachado@gmail.com>
2017-08-04 09:46:06 +0200

    mock modules

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,14 @@
 import sys
 import os
 
+import mock
+
+MOCK_MODULES = ['cplex', 'gurobipy', 'matplotlib']
+
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
+
+
 # If extensions (or modules to document with autodoc) are in another
 # directory, add these directories to sys.path here. If the directory is
 # relative to the documentation root, use os.path.abspath to make it
