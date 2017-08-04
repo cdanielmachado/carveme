60ff34f1d9b54854411472be7b22a9c0c32e105b
Daniel <cdanielmachado@gmail.com>
2017-08-04 10:27:17 +0200

    readthedocs still not happy

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ import os
 
 import mock
 
-MOCK_MODULES = ['cplex', 'gurobipy', '_tkinter']
+MOCK_MODULES = ['cplex', 'gurobipy']
 
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
