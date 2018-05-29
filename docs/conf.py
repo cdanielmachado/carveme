cadb2ba243e7cdfbb0a19a2a319f051840d2d0a1
Daniel <cdanielmachado@gmail.com>
2018-05-29 13:39:04 +0200

    update requirements

--- docs/conf.py
+++ docs/conf.py
@@ -18,7 +18,7 @@ import os
 
 import mock
 
-MOCK_MODULES = ['cplex', 'gurobipy', 'future', 'builtins']
+MOCK_MODULES = ['cplex', 'gurobipy', 'future', 'builtins', 'configparser']
 
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
